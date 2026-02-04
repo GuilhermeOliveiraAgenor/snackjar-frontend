20acbc199c3c71850933cc85b771e0c4627b6ca0
Guilherme Oliveira Agenor <guilherme.oliveira@superfestval.com.br>
2026-02-04 18:00:43 -0300

    feature: add combobox in register form

--- src/modules/recipe/components/register-recipe.tsx
+++ src/modules/recipe/components/register-recipe.tsx
@@ -1,11 +1,19 @@
 "use client"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Combobox, ComboboxContent, ComboboxInput, ComboboxEmpty, ComboboxList, ComboboxItem } from "@/components/ui/combobox";
 import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Pencil } from "lucide-react";
 import { useState } from "react";
+
+const categories = [
+    "Doce",
+    "Salgado",
+    "Almoço",
+    "Jantar",
+    "Café da Manhã",
+  ] as const
 
 export default function RegisterRecipe() {
 
@@ -59,8 +67,24 @@ export default function RegisterRecipe() {
                             }}
                             required
                             />    
-                    <p className="text-sm">minutos</p>
+                            <p className="text-sm">minutos</p>
                             </div>
+                        </Field>
+                        <Field>
+                            <FieldLabel>Categoria</FieldLabel>
+                            <Combobox items={categories}>
+                                <ComboboxInput placeholder="Seleciona a categoria"/>
+                                <ComboboxContent className="left-1/2 -translate-x-1/2">
+                                    <ComboboxEmpty>Itens não encontrados</ComboboxEmpty>
+                                    <ComboboxList>
+                                        {(item) => (
+                                           <ComboboxItem key={item} value={item}>
+                                                {item}
+                                            </ComboboxItem> 
+                                        )}
+                                    </ComboboxList>
+                                </ComboboxContent>
+                            </Combobox>
                         </Field>
                     </FieldGroup>
                 </FieldSet>
