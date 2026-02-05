e9b6b4279d77bd2b6b8b933886d33130e10274d9
Guilherme <guilhermeagenor1012@gmail.com>
2026-02-04 22:24:53 -0300

    feature: add recipe fields in page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,21 +1,20 @@
 import { AppSidebar } from "@/components/menu/side-bar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import RecipeDetails from "@/modules/recipe/components/recipe-details";
 import RegisterRecipe from "@/modules/recipe/components/register-recipe";
 
 export default function Page() {
   return (
     <SidebarProvider
-        style={
-            {
-                "--sidebar-width": "19rem",
-            } as React.CSSProperties
-        }
+      style={
+        {
+          "--sidebar-width": "19rem",
+        } as React.CSSProperties
+      }
     >
-    <AppSidebar/>
-    <SidebarInset>
-        <RegisterRecipe/>
-    </SidebarInset>
+      <AppSidebar />
+      <SidebarInset>
+        <RegisterRecipe />
+      </SidebarInset>
     </SidebarProvider>
   );
 }
--- src/modules/recipe/components/register-recipe.tsx
+++ src/modules/recipe/components/register-recipe.tsx
@@ -1,95 +1,157 @@
-"use client"
+"use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Combobox, ComboboxContent, ComboboxInput, ComboboxEmpty, ComboboxList, ComboboxItem } from "@/components/ui/combobox";
-import { Field, FieldDescription, FieldGroup, FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxInput,
+  ComboboxEmpty,
+  ComboboxList,
+  ComboboxItem,
+} from "@/components/ui/combobox";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+  FieldSet,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 
-const categories = [
-    "Doce",
-    "Salgado",
-    "Almoço",
-    "Jantar",
-    "Café da Manhã",
-  ] as const
+const categories = ["Doce", "Salgado", "Almoço", "Jantar", "Café da Manhã"] as const;
 
 export default function RegisterRecipe() {
-
-  const [text, setText] = useState("")
-  const [preparationTime, setPreparationTime] = useState("")
+  const [text, setText] = useState("");
+  const [preparationTime, setPreparationTime] = useState("");
 
   return (
-    <div>
-        <form>
+    <div className="min-h-screen flex items-center justify-center bg-muted/40 p-4">
+      <Card className="w-full max-w-2xl shadow-lg">
+        <CardHeader>
+          <CardTitle className="text-center">Cadastrar Receita</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <form>
             <FieldGroup>
-                <FieldSet>
-                    <FieldLegend>Cadastrar Receita</FieldLegend>
-                    <FieldDescription>Digite as informações abaixo</FieldDescription>
-                    <FieldGroup>
-                        <Field>
-                            <FieldLabel htmlFor="title">
-                                Título
-                            </FieldLabel>
-                            <Input id="title" placeholder="Bolo de Cenoura" required/>
-                        </Field>
-                        <Field>
-                            <FieldLabel htmlFor="description">
-                                Descrição
-                            </FieldLabel>
-                            <Textarea
-                                placeholder="A receita da Vovó"
-                                maxLength={80}
-                                onChange={(e) => setText(e.target.value)}
-                                className="resize-none"
-                                required
-                            />
-                            <div className="flex justify-end">
-                                <span className="text-xs">
-                                {text.length}/80
-                                </span>
-                            </div>
-                        </Field>
-                        <Field className="-mt-7">
-                            <FieldLabel htmlFor="preparationTime" >Tempo de preparo</FieldLabel>
-                            <div className="flex items-center gap-2">
-                            <Input
-                            type="text"
-                            className="w-32"
-                            placeholder="60"
-                            inputMode="numeric"
-                            maxLength={3}
-                            value={preparationTime}
-                            onChange={(e) => {
-                                const numeric = e.target.value.replace(/\D/g, "");
-                                setPreparationTime(numeric);
-                            }}
-                            required
-                            />    
-                            <p className="text-sm">minutos</p>
-                            </div>
-                        </Field>
-                        <Field>
-                            <FieldLabel>Categoria</FieldLabel>
-                            <Combobox items={categories}>
-                                <ComboboxInput placeholder="Seleciona a categoria"/>
-                                <ComboboxContent className="left-1/2 -translate-x-1/2">
-                                    <ComboboxEmpty>Itens não encontrados</ComboboxEmpty>
-                                    <ComboboxList>
-                                        {(item) => (
-                                           <ComboboxItem key={item} value={item}>
-                                                {item}
-                                            </ComboboxItem> 
-                                        )}
-                                    </ComboboxList>
-                                </ComboboxContent>
-                            </Combobox>
-                        </Field>
-                    </FieldGroup>
-                </FieldSet>
+              <FieldSet>
+                <FieldDescription>Digite as informações abaixo</FieldDescription>
+
+                <FieldGroup>
+                  <Field>
+                    <FieldLabel htmlFor="title">Título</FieldLabel>
+                    <Input id="title" placeholder="Bolo de Cenoura" required />
+                  </Field>
+
+                  <Field>
+                    <FieldLabel htmlFor="description">Descrição</FieldLabel>
+
+                    <Textarea
+                      placeholder="A receita da Vovó"
+                      maxLength={80}
+                      onChange={(e) => setText(e.target.value)}
+                      className="resize-none"
+                      required
+                    />
+
+                    <div className="flex justify-end">
+                      <span className="text-xs text-muted-foreground">{text.length}/80</span>
+                    </div>
+                  </Field>
+
+                  <Field>
+                    <FieldLabel htmlFor="preparationTime">Tempo de preparo</FieldLabel>
+
+                    <div className="flex items-center gap-2">
+                      <Input
+                        type="text"
+                        className="w-32"
+                        placeholder="60"
+                        inputMode="numeric"
+                        maxLength={3}
+                        value={preparationTime}
+                        onChange={(e) => {
+                          const numeric = e.target.value.replace(/\D/g, "");
+                          setPreparationTime(numeric);
+                        }}
+                        required
+                      />
+
+                      <p className="text-sm text-muted-foreground">minutos</p>
+                    </div>
+                  </Field>
+
+                  <Field>
+                    <FieldLabel>Categoria</FieldLabel>
+
+                    <Combobox items={categories}>
+                      <ComboboxInput placeholder="Seleciona a categoria" />
+
+                      <ComboboxContent className="min-w-[var(--radix-popover-trigger-width)]">
+                        <ComboboxEmpty>Itens não encontrados</ComboboxEmpty>
+
+                        <ComboboxList>
+                          {(item) => (
+                            <ComboboxItem key={item} value={item}>
+                              {item}
+                            </ComboboxItem>
+                          )}
+                        </ComboboxList>
+                      </ComboboxContent>
+                    </Combobox>
+                    <FieldSeparator />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="title">Ingrediente</FieldLabel>
+                    <div className="flex flex-1">
+                      <Input id="title" placeholder="Farinha" required />
+                      <Input
+                        id="title"
+                        placeholder="2 xícaras"
+                        required
+                        className="ml-2 w-26 text-center"
+                      />
+                      <Select defaultValue="01">
+                        <SelectTrigger id="unit" className="ml-2 w-36">
+                          <SelectValue placeholder="G" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectLabel>Unidade de Medida</SelectLabel>
+                            <SelectItem value="01">g</SelectItem>
+                            <SelectItem value="02">kg</SelectItem>
+                            <SelectItem value="03">ml</SelectItem>
+                            <SelectItem value="04">l</SelectItem>
+                            <SelectItem value="05">Colher de Sopa</SelectItem>
+                            <SelectItem value="06">Colher de Chá</SelectItem>
+                            <SelectItem value="07">Colher</SelectItem>
+                            <SelectItem value="08">Xícara</SelectItem>
+                            <SelectItem value="09">un</SelectItem>
+                            <SelectItem value="10">Pitada</SelectItem>
+                            <SelectItem value="11">mg</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </Field>
+                </FieldGroup>
+              </FieldSet>
             </FieldGroup>
-        </form>
+          </form>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
