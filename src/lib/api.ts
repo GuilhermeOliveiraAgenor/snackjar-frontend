6e33bfdafd58ac7e7f7970dd6849d19e5a7bcfe3
Guilherme <guilhermeagenor1012@gmail.com>
2026-02-07 13:17:37 -0300

    feature: add api axios

---
+++ src/lib/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { error } from "console";
+
+//create instance axios
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true, // cookie token
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error.response?.data || error);
+  }
+);
