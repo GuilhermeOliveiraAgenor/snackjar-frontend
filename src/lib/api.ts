df1366bd2492956d9f7cb9790d7797b45d937e9d
Guilherme <guilhermeagenor1012@gmail.com>
2026-02-21 19:13:26 -0300

    feature: add zod env validation

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { env } from "./env";
 
 //create instance axios
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: env.NEXT_PUBLIC_API_URL,
   withCredentials: true, // cookie token
   headers: {
     "Content-Type": "application/json",
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,8 +2,20 @@ import { z } from "zod";
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string().url(),
+
+  NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 });
 
-export const env = envSchema.parse({
+const _env = {
   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-});
+  NODE_ENV: process.env.NODE_ENV,
+  NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+};
+
+const parsed = envSchema.safeParse(_env);
+
+if (!parsed.success) {
+  throw new Error("Invalid environment variables");
+}
+
+export const env = parsed.data;
